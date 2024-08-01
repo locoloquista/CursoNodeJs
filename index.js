2f2d3e1db9d401d5558ffa1b78ab7d6a2152a0c6
Locoloquista <tomas_garcia_cornejo@yahoo.com>
2024-07-31 22:24:18 -0600

    inicio de curso
    Aprendiendo CommonJS module export

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log('Hola mundo')
+
+//Varaible global de donde nacen todas las funciones "por defecto"
+//console.log(globalThis)
+
+//Common JS module export -> forma clásica de exportar módulos
+//CommonJS require module
+const {sum}=require('./sum')
+console.log(sum(6,8))
+
+
+
