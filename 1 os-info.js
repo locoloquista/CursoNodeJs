2e5f469c190e9246e8920354931380cda8d774ef
Locoloquista <tomas_garcia_cornejo@yahoo.com>
2024-07-31 23:01:50 -0600

    Aprendiendo a utilizar modulos nativos de Node con CommonJs

---
+++ 1 os-info.js
@@ -0,0 +1,15 @@
+//Modulos nativos de Node con CommonJs
+
+const os = require('node:os')
+
+console.log('INFORMACION DEL SISTEMA OPERATIVO')
+console.log('---------------------------------')
+
+console.log('Nombre del sistema operativo: ', os.platform())
+console.log('Version del sistema operativo: ', os.release())
+console.log('Arquitectura: ', os.arch())
+console.log('Memoria libre: ', os.freemem()/1024/1024)
+console.log('Memoria total: ', os.totalmem()/1024/1024)
+console.log('CPUs: ', os.cpus())
+console.log('Horas trabajando: ', os.uptime()/60/60)
+
