64c9cc5ad7bee58cbdeb8354f58085eccdb940cf
Locoloquista <tomas_garcia_cornejo@yahoo.com>
2024-10-01 23:15:42 -0600

    Técnicas para corregir problemas de cors

--- lesson3/app.js
+++ lesson3/app.js
@@ -7,11 +7,22 @@ const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://movies.com'
+]
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hola mundo' })
 })
 
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+
   const { genre } = req.query // cuando enviamo url http://localhost:1234/movies?genre=Action
   if (genre) {
     const filteredMovies = movies.filter(
@@ -48,6 +59,24 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie)
 })
 
+app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie delete' })
+})
+
 // Modificar una parte de la pelicula
 app.patch('/movies/:id', (req, res) => {
   const { id } = req.params
@@ -71,6 +100,16 @@ app.patch('/movies/:id', (req, res) => {
   movies[movieIndex] = updateMovie
 
   res.status(200).json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE')
+  }
+
+  res.send(200)
 })
 
 const PORT = process.env.PORT ?? 1234
